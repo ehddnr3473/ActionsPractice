05a47dd0121333bbb0856eb9b89263e504d95110
Donguk Kim <ehddnr73@naver.com>
2023-05-04 15:03:21 +0900

    ActionsPractice, Swift Package 생성

---
+++ Sources/ActionsPractice/ActionsPractice.swift
@@ -0,0 +1,6 @@
+public struct ActionsPractice {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ActionsPracticeTests/ActionsPracticeTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ActionsPractice
+
+final class ActionsPracticeTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ActionsPractice().text, "Hello, World!")
+    }
+}
