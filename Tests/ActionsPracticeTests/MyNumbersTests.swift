34b6ba0ac1c55b4b56ab44da4e087688af4597ee
Donguk Kim <ehddnr73@naver.com>
2023-05-05 01:16:00 +0900

    MyNumbersTests.swift 생성
    - MyNumbers struct에 대한 테스트 코드 작성

---
+++ Tests/ActionsPracticeTests/MyNumbersTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import ActionsPractice
+
+final class MyNumbersTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertEqual(MyNumbers().isThirty, true)
+    }
+}
